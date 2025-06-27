543cdff944d1aaebc9a26e69a780e429d9338e94
Y3abtsega <yeabtsega.aragaw@bitscollege.edu.et>
2025-06-27 11:17:52 +0300

    added tests for adding product

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cart) { Cart.create }
+  let(:product) { Product.create(name: "Keyboard", price: 100) }
+
+  describe "#add_product" do
+    it "adds a product to the cart" do
+      cart.add_product(product.id)
+      expect(cart.cart_items.count).to eq(1)
+      expect(cart.cart_items.first.quantity).to eq(1)
+    end
+  end
 end
