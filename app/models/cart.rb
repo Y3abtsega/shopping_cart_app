ccef20e1672d58f0e6b9e017d1a6a33bc2163939
Y3abtsega <yeabtsega.aragaw@bitscollege.edu.et>
2025-06-27 10:53:46 +0300

    added revome product functionality

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,4 +8,17 @@ class Cart < ApplicationRecord
     item.quantity += 1
     item.save
   end
+
+  def remove_product(product_id)
+    item = cart_items.find_by(product_id: product_id)
+    return unless item
+
+    if item.quantity > 1
+      item.quantity -= 1
+      item.save
+    else
+      item.destroy
+    end
+  end
+
 end
