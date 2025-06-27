05e99e1c4c6921afb98c012f4075ac1c9013df14
Y3abtsega <yeabtsega.aragaw@bitscollege.edu.et>
2025-06-27 10:51:01 +0300

    added add functionality

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,11 @@
 class Cart < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
+
+  def add_product(product_id)
+    item = cart_items.find_or_initialize_by(product_id: product_id)
+    item.quantity ||= 0
+    item.quantity += 1
+    item.save
+  end
 end
