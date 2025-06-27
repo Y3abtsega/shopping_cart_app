fa6b8c0b56af2ccac5be50653b6ee50cf66482fc
Y3abtsega <yeabtsega.aragaw@bitscollege.edu.et>
2025-06-27 11:12:58 +0300

    added tests for product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name and price" do
+    expect(Product.new(name: "Mouse", price: 20)).to be_valid
+  end
+
+  it "is invalid without a name" do
+    expect(Product.new(price: 20)).not_to be_valid
+  end
+
+  it "is invalid without a price" do
+    expect(Product.new(name: "Mouse")).not_to be_valid
+  end
 end
